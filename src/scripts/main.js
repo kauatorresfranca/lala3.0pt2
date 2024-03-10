b52e33eb80e3c92b71368bd72af17725222e1a60
kaua torres <kauatorresfranca2@gmail.com>
2024-03-10 12:19:17 -0300

    mudanças

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('olá,mundo')
+AOS.init();
+
+const dataDoEvento = new Date("dec 12, 2024 19:00:00")
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date()
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / (1000 * 3600 * 24))
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 3600 * 24)) / (1000 * 60 * 60))
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60 * 60)) / (1000 *60))
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % (1000 *60)) / 1000)
+
+    console.log(diasAteOEvento)
+    console.log(horasAteOEvento)
+    console.log(minutosAteOEvento)
+    console.log(segundosAteOEvento)
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if(distanciaAteOEvento < 0){
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado'
+    }
+},1000)
